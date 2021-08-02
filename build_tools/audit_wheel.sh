fcba7e76cd8c73eb01b9777043cf88060421d8a7
Vlad Niculae <vlad@vene.ro>
2021-08-02 19:28:56 +0200

    try custom audit script

---
+++ build_tools/audit_wheel.sh
@@ -0,0 +1,8 @@
+set -x
+LP=`python -c "from torch.utils.cpp_extension import library_paths as lp; print(':'.join(lp()))"`
+export LD_LIBRARY_PATH=$LP
+auditwheel repair -w $1 $2
+#export LIBRARY_PATH=$LP
+#export DYLD_LIBRARY_PATH=$LP
+#export LD_RUNPATH_SEARCH_PATH=$LP
+#export DYLD_FALLBACK_LIBRARY_PATH=$LP
