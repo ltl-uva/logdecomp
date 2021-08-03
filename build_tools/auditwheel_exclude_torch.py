7c2054344af351282dce670461f3ab2ec6534cc4
Vlad Niculae <vlad@vene.ro>
2021-08-03 11:07:51 +0200

    proper attribution

--- build_tools/auditwheel_exclude_torch.py
+++ build_tools/auditwheel_exclude_torch.py
@@ -1,11 +1,15 @@
-# -*- coding: utf-8 -*-
+"""Monkey-patch auditwheel policies to allow torch libraries.
+
+Idea simplified from the insipration source:
+<https://github.com/DIPlib/diplib/blob/bca5333236f42e02046db8684af61173f200086e/tools/travis/auditwheel>
+"""
+
 import re
 import sys
 
 from auditwheel.main import main
 from auditwheel.policy import _POLICIES as POLICIES
 
-# libjvm is loaded dynamically; do not include it
 for p in POLICIES:
     if p['name'].startswith('manylinux'):
         p['lib_whitelist'].extend(['libtorch.so', 'libc10.so', 'libtorch_cpu.so', 'libtorch_python.so'])
